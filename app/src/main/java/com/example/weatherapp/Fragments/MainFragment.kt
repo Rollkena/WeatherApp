4493cb171880cd773cb2c8c48716d35106165b31
Rollkena <rollkena@gmail.com>
2023-05-23 17:15:09 +0500

    Create function for updating main card

--- app/src/main/java/com/example/weatherapp/Fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/Fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.Fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +11,22 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherapp.Adapters.VpAdapter
+import com.example.weatherapp.Adapters.WeatherModel
+import com.example.weatherapp.MainViewModel
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import com.squareup.picasso.Picasso
+import org.json.JSONObject
+import java.util.Objects
+
+
+const val API_KEY = "80020229fbfa4c9ba8b93218231504"
 
 class MainFragment : Fragment() {
     private val fList = listOf(
@@ -26,6 +39,7 @@ class MainFragment : Fragment() {
     )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
+    private val model: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,33 +54,122 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        updateCurrentCard()
+        requestWeatherData("London")
     }
 
     //инициализируем отображение списков во вкладках и переключение
-    private fun init() = with(binding){
+    private fun init() = with(binding) {
         val adapter = VpAdapter(activity as FragmentActivity, fList)
         vp.adapter = adapter
-        TabLayoutMediator(tabLayout4, vp){
-            tab, pos -> tab.text = tLits[pos]
+        TabLayoutMediator(tabLayout4, vp) { tab, pos ->
+            tab.text = tLits[pos]
         }.attach()
     }
 
+    //обновление данных главной карточки
+    private fun updateCurrentCard() = with(binding){
+        model.liveDataCurrent.observe(viewLifecycleOwner){
+            val maxMinTemp = "${it.maxTemp}C/${it.minTemp}C"
+            tvData.text = it.time
+            tvCity.text = it.city
+            tvCurrentTemp.text = it.currentTemp
+            tvCondition.text = it.condition
+            tvMaxMin.text = maxMinTemp
+            Picasso.get().load("https:" + it.imageUrl).into(imWeather)
+        }
+    }
+
     //проверка наличия разрешения
-    private fun permissionListener(){
+    private fun permissionListener() {
         pLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()){
+            ActivityResultContracts.RequestPermission()
+        ) {
             Toast.makeText(activity, "Perm is $it", Toast.LENGTH_LONG).show()
         }
     }
 
     //запрос разрешения, если его нет
-    private fun checkPermission(){
-        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
         //TODO сделать отображение, что функционал урезан, если пользователь отклонил запрос
     }
+
+    //Запрос серверу по городу
+    private fun requestWeatherData(city: String) {
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "3" +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                    result -> parseWeatherData(result)
+            },
+            {
+                    error ->
+                Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
+    }
+
+    //общая функция парсинга
+    private fun parseWeatherData(result: String) {
+        val mainObject = JSONObject(result)
+        val list = parseDays(mainObject)
+        parseCurrentData(mainObject, list[0])
+    }
+
+
+    //парсим полученый результат от сервера для списка дней
+    private fun parseDays(mainObject: JSONObject) : List<WeatherModel> {
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
+        val name = mainObject.getJSONObject("location").getString("name")
+        for (i in 0 until daysArray.length()){
+            val day = daysArray[i] as JSONObject
+            val item = WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("text"),
+                "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("icon"),
+                day.getJSONArray("hour").toString()
+            )
+            list.add(item)
+        }
+        return list
+    }
+
+    //парсим полученый результат от сервера для карточки
+    private fun parseCurrentData(mainObject: JSONObject, weatherItem: WeatherModel){
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            weatherItem.maxTemp,
+            weatherItem.minTemp,
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("icon"),
+            weatherItem.hours
+        )
+        model.liveDataCurrent.value = item
+        Log.d("MyLog", "City: ${item.maxTemp}, time: ${item.hours}")
+    }
+
 
     companion object {
         @JvmStatic
